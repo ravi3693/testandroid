4c680277e579ade97294ca8954950d00c5cca761
Ravikanth <ravi@theinnerhour.com>
2019-04-03 15:50:10 +0530

    added changes to babel configuration

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   presets: [
-    'module:metro-react-native-babel-preset',
-    "@babel/preset-env",
-    "@babel/preset-react"
+    'module:metro-react-native-babel-preset'
   ],
 };
